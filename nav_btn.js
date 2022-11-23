a10d38a5e2271b04081c73bdfe02fa744145dea9
Datamayor <106524048+Datamayor@users.noreply.github.com>
2022-11-23 13:34:59 +0100

    cooking

---
+++ nav_btn.js
@@ -0,0 +1,21 @@
+const primaryNav = document.querySelector("#nav_btn");
+const navToggle = document.querySelector (".mobile-nav-toggle");
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible')
+    
+    if (visibility === "false"){
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    }else if (visibility === "true"){
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+} );
+
+let toggle = document.querySelector(".toggle");
+
+
+function Animatedtoggle(){
+    toggle.classList.toggle("active");
+}
